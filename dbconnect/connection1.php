ccf123a00050bfaa54673b1211ecd7923ce276c4
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-31 17:26:44 +0900

    11

--- dbconnect/connection1.php
+++ dbconnect/connection1.php
@@ -5,8 +5,14 @@ $dbc = mysqli_connect("localhost", "root", "root506", "employees", 3306);
 // var_dump($dbc);
 
 //request
-
-$result_query_emp_no = mysqli_query($dbc, "SELECT emp_no, concat(last_name, ' ' , first_name) as full_name, gender, birth_date FROM employees Where emp_no <= 10005;");
+$sql =
+"SELECT emp_no,
+        concat(last_name, ' ' , first_name) as full_name,
+        gender, 
+        birth_date 
+        FROM employees 
+        Where emp_no <= 10005;";
+$result_query_emp_no = mysqli_query($dbc, $sql);
 
 if (mysqli_num_rows($result_query_emp_no) > 0) {
     while ($row = mysqli_fetch_assoc($result_query_emp_no)) {
