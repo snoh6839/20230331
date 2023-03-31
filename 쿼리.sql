6ca590f1fb8064a7f7ab9534cbc25cb098a04666
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-31 09:36:34 +0900

    create table

--- 쿼리.sql
+++ 쿼리.sql
@@ -4,13 +4,13 @@
 -- password VARCHAR(50) NOT NULL
 -- );
 
-CREATE TABLE todo_items (
-Id_no INT PRIMARY KEY AUTO_INCREMENT,
-member_id INT NOT NULL,
-title VARCHAR(100) NOT NULL,
-description TEXT,
-start_date DATE,
-due_date DATE,
-status ENUM('Incomplete', 'Complete') NOT NULL DEFAULT 'Incomplete',
-FOREIGN KEY (member_id) REFERENCES todo_members(id_no)
-);
+-- CREATE TABLE todo_items (
+-- Id_no INT PRIMARY KEY AUTO_INCREMENT,
+-- member_id INT NOT NULL,
+-- title VARCHAR(100) NOT NULL,
+-- description TEXT,
+-- start_date DATE,
+-- due_date DATE,
+-- status ENUM('Incomplete', 'Complete') NOT NULL DEFAULT 'Incomplete',
+-- FOREIGN KEY (member_id) REFERENCES todo_members(id_no)
+-- );
