7def19dead6439c3ae18b36c4c2e1465e3555c84
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-31 12:40:39 +0900

    push

--- file2.php
+++ file2.php
@@ -18,23 +18,23 @@
 
 // fclose($myfile);
 
-$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+// $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
 
-for ($i = 2; $i <= 9; $i++) {
-    $text = $i . "단 입니다.\n";
-    fputs($myfile, $text);
+// for ($i = 2; $i <= 9; $i++) {
+//     $text = $i . "단 입니다.\n";
+//     fputs($myfile, $text);
 
-    for ($j = 1; $j <= 9; $j++) {
-        $text = $i . " X " . $j . " = " . ($i * $j) . "\n";
-        fputs($myfile, $text);
-    }
-    $text = "\n";
-    fputs($myfile, $text);
-}
+//     for ($j = 1; $j <= 9; $j++) {
+//         $text = $i . " X " . $j . " = " . ($i * $j) . "\n";
+//         fputs($myfile, $text);
+//     }
+//     $text = "\n";
+//     fputs($myfile, $text);
+// }
 
-fclose($myfile);
+// fclose($myfile);
 
-$myfile2 = fopen("newfile2.txt", "a") or die("Unable to open file!");
+$myfile2 = fopen("newfile3.txt", "a") or die("Unable to open file!");
 
 $text_arr = array();
 for ($i = 2; $i <= 9; $i++) {
